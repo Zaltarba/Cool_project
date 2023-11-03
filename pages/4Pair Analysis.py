fc1159aff20bc94eeb22820890638837b5afed3e
Zaltarba <122260822+Zaltarba@users.noreply.github.com>
2023-11-03 17:40:32 +0100

    Update 4Pair Analysis.py

--- pages/4Pair Analysis.py
+++ pages/4Pair Analysis.py
@@ -9,12 +9,12 @@ st.sidebar.success("Select a feature above.")
 
 st.write("# Pair Analysis")
 
-col1, col2 = st.column(2)
+col1, col2 = st.columns(2)
 with col1:
     ticker_1 = st.text_input('Enter first ticker to be studied, e.g. MA,META,V,AMZN,JPM,BA', '').upper()
 with col2:
     ticker_2 = st.text_input('Enter second ticker to be studied, e.g. MA,META,V,AMZN,JPM,BA', '').upper()
-    
+
 hide_streamlit_style = """
             <style>
             #MainMenu {visibility: hidden;}
