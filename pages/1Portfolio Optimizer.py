0cff24000f708def8b4b317ab18d30ae688df056
Zaltarba <122260822+Zaltarba@users.noreply.github.com>
2023-11-04 12:23:49 +0100

    Update 1Portfolio Optimizer.py

--- pages/1Portfolio Optimizer.py
+++ pages/1Portfolio Optimizer.py
@@ -14,19 +14,19 @@ from datetime import datetime
 from io import BytesIO
 
 st.set_page_config(
-    page_title="Portfolio Optimizer",
-    layout="wide",
-    page_icon="📈",
+	page_title="Portfolio Optimizer",
+	layout="wide",
+	page_icon="📈",
 )
 st.sidebar.success("Select a feature above.")
 st.title("Portfolio Optimizer 🚀")	
 
 # Define styles for matplotlib and seaborn inside functions
 
-def plot_cum_returns(data, title):    
-    daily_cum_returns = (1 + data.dropna().pct_change()).cumprod()*100
-    fig = px.line(daily_cum_returns, title=title)
-    return fig
+def plot_cum_returns(data, title):	
+	daily_cum_returns = (1 + data.dropna().pct_change()).cumprod()*100
+	fig = px.line(daily_cum_returns, title=title)
+	return fig
 
 
 def plot_efficient_frontier_and_max_sharpe(mu, S, r:float): 
@@ -49,34 +49,34 @@ def plot_efficient_frontier_and_max_sharpe(mu, S, r:float):
 	# Output
 	ax.legend()
 	return fig
-    
-    # Efficient frontier plotting code...
+		
+	# Efficient frontier plotting code...
 
 # Main layout
 col1, col2, col3, col4 = st.columns([1, 1, 1, 1])
 with col1:
-    start_date = st.date_input("Start Date", datetime(2013, 1, 1))
+	start_date = st.date_input("Start Date", datetime(2013, 1, 1))
 with col2:
-    end_date = st.date_input("End Date")  # Defaults to current date
+	end_date = st.date_input("End Date")  # Defaults to current date
 with col3:
-    r = st.number_input("Risk-free rate", value=0.02)
+	r = st.number_input("Risk-free rate", value=0.02)
 with col4:
-    expected_return_method = st.selectbox(
-        "Expected Return Method", 
-        ["Mean historical return", "Exponentially-weighted mean historical return"]
-    )
+	expected_return_method = st.selectbox(
+		"Expected Return Method", 
+		["Mean historical return", "Exponentially-weighted mean historical return"]
+	)
 
 if expected_return_method == "Exponentially-weighted mean historical return":
-    span = st.slider("Time span for the EMA", min_value=30, max_value=1000, value=500, step=10)
+	span = st.slider("Time span for the EMA", min_value=30, max_value=1000, value=500, step=10)
 
 tickers_string = st.text_input(
-    'Enter stock tickers separated by commas WITHOUT spaces, e.g. "MA,META,V,AMZN,JPM,BA"',
-    ''
+	'Enter stock tickers separated by commas WITHOUT spaces, e.g. "MA,META,V,AMZN,JPM,BA"',
+	''
 ).upper()
 tickers = tickers_string.split(',')
 
 if st.button('Analyze Portfolio'):
-    try:
+	try :
 		stocks_df = yf.download(tickers, start=start_date, end=end_date)['Adj Close']  # Get Stock Prices using pandas_datareader Library	
 		fig_price = px.line(stocks_df, title='Price of Individual Stocks')  # Plot Individual Stock Prices
 		# Plot Individual Cumulative Returns
@@ -111,44 +111,44 @@ if st.button('Analyze Portfolio'):
 		# Plot Cumulative Returns of Optimized Portfolio
 		fig_cum_returns_optimized = plot_cum_returns(stocks_df['Optimized Portfolio'], 'Cumulative Returns of Optimized Portfolio Starting with $100')
 		
-        # Display everything on Streamlit using tabs
-        tab1, tab2, tab3, tab4 = st.tabs(["Prices", "Correlations", "Returns", "Optimized Portfolio"])
-    	with tab1:
-            fig_price = px.line(stocks_df, title='Price of Individual Stocks')
-            st.plotly_chart(fig_price, use_container_width=True)
-        
-        with tab2:
-            st.plotly_chart(fig_corr, use_container_width=True)
-        
-        with tab3:
-            st.plotly_chart(fig_cum_returns, use_container_width=True)
-        
-        with tab4:
-            st.subheader('Portfolio Weights and Performance')
-            st.dataframe(weights_df)
-            st.plotly_chart(fig_cum_returns_optimized, use_container_width=True)
-            st.image(fig_efficient_frontier.getvalue())
-            
-            st.metric(label="Expected Annual Return", value=f"{expected_annual_return:.2%}")
-            st.metric(label="Annual Volatility", value=f"{annual_volatility:.2%}")
-            st.metric(label="Sharpe Ratio", value=f"{sharpe_ratio:.2f}")     
-    except ValueError:
-    	st.error('Please enter valid stock tickers separated by commas WITHOUT spaces.')
+		# Display everything on Streamlit using tabs
+		tab1, tab2, tab3, tab4 = st.tabs(["Prices", "Correlations", "Returns", "Optimized Portfolio"])
+		with tab1:
+			fig_price = px.line(stocks_df, title='Price of Individual Stocks')
+			st.plotly_chart(fig_price, use_container_width=True)
+		
+		with tab2:
+			st.plotly_chart(fig_corr, use_container_width=True)
+		
+		with tab3:
+			st.plotly_chart(fig_cum_returns, use_container_width=True)
+		
+		with tab4:
+			st.subheader('Portfolio Weights and Performance')
+			st.dataframe(weights_df)
+			st.plotly_chart(fig_cum_returns_optimized, use_container_width=True)
+			st.image(fig_efficient_frontier.getvalue())
+				 
+			st.metric(label="Expected Annual Return", value=f"{expected_annual_return:.2%}")
+			st.metric(label="Annual Volatility", value=f"{annual_volatility:.2%}")
+			st.metric(label="Sharpe Ratio", value=f"{sharpe_ratio:.2f}")	 
+	except ValueError:
+		st.error('Please enter valid stock tickers separated by commas WITHOUT spaces.')
 
 # Custom styles
 st.markdown(
-    """
-    <style>
-    #MainMenu {visibility: hidden;}
-    footer {visibility: hidden;}
-    .stTabs > .tablist > li > button {
-        padding: 10px 20px;
-        border: none;
-        margin: 0 2px;
-        font-size: 16px;
-        font-weight: bold;
-    }
-    </style>
-    """, 
-    unsafe_allow_html=True
+	"""
+	<style>
+	#MainMenu {visibility: hidden;}
+	footer {visibility: hidden;}
+	.stTabs > .tablist > li > button {
+		padding: 10px 20px;
+		border: none;
+		margin: 0 2px;
+		font-size: 16px;
+		font-weight: bold;
+	}
+	</style>
+	""", 
+	unsafe_allow_html=True
 )
