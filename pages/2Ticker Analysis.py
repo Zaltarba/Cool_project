04cd9b123e169e47d31facc2e08fe04c845ff83f
Zaltarba <122260822+Zaltarba@users.noreply.github.com>
2023-11-05 22:52:46 +0100

    Update 2Ticker Analysis.py

--- pages/2Ticker Analysis.py
+++ pages/2Ticker Analysis.py
@@ -9,7 +9,7 @@ st.set_page_config(page_title="Ticker Analysis", page_icon="📈")
 st.write("# Ticker Analysis")
 
 # Input for ticker symbol
-ticker = st.text_input('Enter ticker to be studied, e.g. MA, META, V, AMZN, JPM, BA', '').upper()
+ticker = st.text_input('Enter ticker to be studied, e.g. MSFT, AAPL, GOOGL', '').upper()
 
 # Hide default Streamlit style
 hide_streamlit_style = """
@@ -24,22 +24,32 @@ st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 def display_ticker_data(ticker_symbol):
     if ticker_symbol:
         try:
-            # Fetch data
-            data = yf.Ticker(ticker_symbol)
-            hist = data.history(period="1mo")
-
-            # Check if data is retrieved
-            if hist.empty:
-                st.error("No data found for the ticker symbol.")
-                return
+            # Import the ticker and its information
+            stock = yf.Ticker(ticker_symbol)
 
             # Display basic info
             st.write(f"## Fundamental Analysis of {ticker_symbol}")
-            st.table(data.info)
 
-            # Display interactive chart
-            fig = px.line(hist, x=hist.index, y="Close", title=f'{ticker_symbol} Closing Prices')
-            st.plotly_chart(fig)
+            # Extract and display company information
+            st.subheader("Company Information:")
+            st.json(stock.info)
+
+            # Extract and display dividends
+            st.subheader("Dividends:")
+            st.line_chart(stock.dividends)
+
+            # Extract and display stock splits
+            st.subheader("Stock Splits:")
+            st.table(stock.splits)
+
+            # Extracts and displays some financials 
+            st.subheader("Financials:")
+            st.table(stock.financials)
+
+            # Extracts and displays the balance sheet
+            st.subheader("Balance Sheet:")
+            st.table(stock.balance_sheet)
+
         except Exception as e:
             st.error(f"An error occurred: {e}")
 
