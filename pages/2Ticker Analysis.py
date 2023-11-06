ac952ad62d5096761489a95c33f9b86a3863634a
Zaltarba <122260822+Zaltarba@users.noreply.github.com>
2023-11-06 22:58:44 +0100

    Update 2Ticker Analysis.py

--- pages/2Ticker Analysis.py
+++ pages/2Ticker Analysis.py
@@ -20,6 +20,11 @@ ticker = st.selectbox(
     ticker_options, 
 )
 
+if 'reddit_news_data' not in st.session_state:
+    st.session_state['reddit_news_data'] = None
+
+if 'comment_analysis_data' not in st.session_state:
+    st.session_state['comment_analysis_data'] = None
 # Checkbox to run fundamental analysis
 run_fundamental_analysis = st.button('Run Fundamental Analysis')
 
@@ -107,7 +112,7 @@ def get_reddit_news(ticker_symbol, subreddits=None):
 run_reddit_analysis = st.button('Run Reddit Analysis')
 
 # Run the analysis if the checkbox is checked
-if run_reddit_analysis:
+if st.session_state['reddit_news_data']:
     st.write("## Latest News on Reddit")
     try:
         news_items = get_reddit_news(ticker, selected_subreddits)
@@ -148,7 +153,7 @@ def get_comment_data(ticker_symbol, subreddit_list, post_limit=10, comment_limit
 
 run_analysis = st.button('Run Analysis')
 
-if run_analysis:
+if st.session_state['comment_analysis_data']:
     if not selected_subreddits:
         st.error("Please select at least one subreddit.")
     else:
