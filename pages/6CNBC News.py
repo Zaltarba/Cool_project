3105d56d318db6591245db401854b8586778eb84
Zaltarba <122260822+Zaltarba@users.noreply.github.com>
2023-11-14 12:36:24 +0100

    Update 6CNBC News.py

--- pages/6CNBC News.py
+++ pages/6CNBC News.py
@@ -21,7 +21,8 @@ col1, col2, col3, col4 = st.tabs(feeds.keys())
 def display_feed(column, feed_url):
     feed = feedparser.parse(feed_url)
     text = " ".join([entry.title for entry in feed.entries])
-    #text += " ".join([entry.summary for entry in feed.entries])
+    column.write(text)
+    # text += " ".join([entry.summary for entry in feed.entries])
     # Create a word cloud object with desired parameters
     wordcloud = WordCloud(width=800, height=400, background_color='black', colormap='Pastel1').generate(" ".join(text))
                 
