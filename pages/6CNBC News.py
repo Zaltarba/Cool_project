a8ce150602782143168bd4aea4cada9df29bafd0
Zaltarba <122260822+Zaltarba@users.noreply.github.com>
2023-11-14 12:42:29 +0100

    Update 6CNBC News.py

--- pages/6CNBC News.py
+++ pages/6CNBC News.py
@@ -20,11 +20,11 @@ col1, col2, col3, col4 = st.tabs(feeds.keys())
 # Function to display a single feed
 def display_feed(column, feed_url):
     feed = feedparser.parse(feed_url)
-    text = " ".join([entry.title for entry in feed.entries])
+    texte = " ".join([entry.title for entry in feed.entries])
     column.write(text)
-    # text += " ".join([entry.summary for entry in feed.entries])
+    text += " ".join([entry.summary for entry in feed.entries])
     # Create a word cloud object with desired parameters
-    wordcloud = WordCloud(width=800, height=400, background_color='black', colormap='Pastel1').generate(" ".join(text))
+    wordcloud = WordCloud(width=800, height=400, background_color='black', colormap='Pastel1').generate(text)
                 
     # Set up the figure size and layout with a black background
     fig, ax = plt.subplots(figsize=(10, 5))
