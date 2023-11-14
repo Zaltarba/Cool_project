8fb23cd84f8adf311aa5583d048505a95fe2868e
Zaltarba <122260822+Zaltarba@users.noreply.github.com>
2023-11-14 13:33:09 +0100

    Update 6CNBC News.py

--- pages/6CNBC News.py
+++ pages/6CNBC News.py
@@ -20,18 +20,24 @@ st.title("CNBC RSS Feed Reader")
 columns = st.tabs(feeds.keys())
 
 # Function to display a single feed
-def display_feed(column, feed_url):
+def display_feed(column, feed_url, feed_key):
+
     feed = feedparser.parse(feed_url)
-    try:
-        text = " ".join([entry.title for entry in feed.entries])
-        is_header = True
-    except AttributeError:
-        is_header = False
-    try:
-        text += " ".join([entry.summary for entry in feed.entries])
-        is_summary = True
-    except AttributeError:
-        is_summary = False
+    
+    # Check if the feed key is in session state, if not initialize it
+    if feed_key not in st.session_state:
+        st.session_state[feed_key] = 1  # Displaying the first news item initially
+    displayed_items = st.session_state[feed_key]
+    # Process text for WordCloud
+    for entry in feed.entries[:displayed_items]:
+        try:
+            text = " ".join(entry.title)
+        except AttributeError:
+            pass
+        try:
+            text += " " + " ".join(entry.summary)
+        except AttributeError:
+            pass
 
     # Create a word cloud object with desired parameters
     wordcloud = WordCloud(width=800, height=400, background_color='black', colormap='Pastel1').generate(text)            
@@ -45,7 +51,8 @@ def display_feed(column, feed_url):
     # Clear the current figure to ensure it does not interfere with future plots
     plt.clf()
 
-    for entry in feed.entries:
+    # Display the limited number of feed entries
+    for entry in feed.entries[:displayed_items]:
         try:
             column.subheader(entry.title)
             try:
@@ -56,12 +63,15 @@ def display_feed(column, feed_url):
         except AttributeError:
             pass
 
+    # Button to load more news
+    if column.button("Show More", key=feed_key):
+        st.session_state[feed_key] += 5
+
 # Displaying feeds in each column
 for i, col in enumerate(columns):
-
     with col:
         header = list(feeds.keys())[i]
         st.header(header)
-        display_feed(col, feeds[header])
+        display_feed(col, feeds[header], header)
 
 # Add more columns/sections as needed
